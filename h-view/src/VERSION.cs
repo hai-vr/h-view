41ed8ab548db6fb393af855f6a4257ee5c4bdddc
Haï~ <hai-vr@users.noreply.github.com>
2024-09-07 03:41:09 +0200

    In GitHub Actions, use package.json version as assembly version: - In GitHub Actions, pass the package.json version number to the build. - This is done through the assembly version of the .csproj - In the code, source the version number from the executing assembly.

--- h-view/src/VERSION.cs
+++ h-view/src/VERSION.cs
@@ -1,7 +1,16 @@
-﻿namespace Hai.HView.Core;
+﻿using System.Globalization;
+using System.Reflection;
+
+namespace Hai.HView.Core;
 
 public class VERSION
 {
-    // TODO: Somehow pull the version number out of the package.json file.
-    public static string version = "v1.0.0";
+    // ReSharper disable once InconsistentNaming
+    public static string version { get; private set; }
+
+    static VERSION()
+    {
+        var v = Assembly.GetExecutingAssembly().GetName().Version;
+        version = string.Format(CultureInfo.InvariantCulture, "v{0}.{1}.{2}", v.Major, v.Minor, v.Build);
+    }
 }
