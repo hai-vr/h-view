c328cd58ae669235dfaef3c8ed4397fdff67a8ec
Haï~ <hai-vr@users.noreply.github.com>
2024-09-26 06:09:12 +0200

    Replace conditional compilation with constants on release builds: - When making a release build, replace conditional compilation with constants to prune unused code paths.

--- h-view/src/ConditionalCompilation.cs
+++ h-view/src/ConditionalCompilation.cs
@@ -3,24 +3,49 @@
 // ReSharper disable UnusedAutoPropertyAccessor.Local
 public static class ConditionalCompilation
 {
-    public static bool IncludesOpenVR { get; private set; }
-    public static bool RegisterManifest { get; private set; }
-    public static bool IncludesSteamworks { get; private set; }
-    public static bool CookiesSupported { get; private set; }
+// Use static bool properties to avoid triggering "Heuristically Unreachable Code" warnings in editor,
+// but use constants in releases.
+#if USE_CONSTS_IN_RELEASE
+    public const bool IncludesOpenVR = _IncludesOpenVR;
+    public const bool RegisterManifest = _RegisterManifest;
+    public const bool IncludesSteamworks = _IncludesSteamworks;
+    public const bool CookiesSupported = _CookiesSupported;
+    public const bool EnableFakeVrcOsc = _EnableFakeVrcOsc;
+#else
+    public static bool IncludesOpenVR => _IncludesOpenVR;
+    public static bool RegisterManifest => _RegisterManifest;
+    public static bool IncludesSteamworks => _IncludesSteamworks;
+    public static bool CookiesSupported => _CookiesSupported;
+    public static bool EnableFakeVrcOsc => _EnableFakeVrcOsc;
+#endif
     
-    static ConditionalCompilation()
-    {
 #if INCLUDES_OPENVR
-        IncludesOpenVR = true;
+    private const bool _IncludesOpenVR = true;
+#else
+    private const bool _IncludesOpenVR = false;
 #endif
+    
 #if REGISTER_MANIFEST
-        RegisterManifest = true;
+    private const bool _RegisterManifest = true;
+#else
+    private const bool _RegisterManifest = false;
 #endif
+    
 #if INCLUDES_STEAMWORKS
-        IncludesSteamworks = true;
+    private const bool _IncludesSteamworks = true;
+#else
+    private const bool _IncludesSteamworks = false;
 #endif
+    
 #if COOKIES_SUPPORTED
-        CookiesSupported = true;
+    private const bool _CookiesSupported = true;
+#else
+    private const bool _CookiesSupported = false;
 #endif
-    }
+    
+#if ENABLE_FAKE_VRC_OSC
+    private const bool _EnableFakeVrcOsc = true;
+#else
+    private const bool _EnableFakeVrcOsc = false;
+#endif
 }
