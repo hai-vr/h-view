5b8fb2a925474af6fabfcff3198c66bb6f937a94
Haï~ <hai-vr@users.noreply.github.com>
2024-10-02 15:35:11 +0200

    Clean up the main program scaffolding.

--- h-view/Program.cs
+++ h-view/Program.cs
@@ -8,81 +8,105 @@ using Hai.HView.OSC.PretendToBeVRC;
 using Hai.HView.OVR;
 using Hai.HView.SavedData;
 
-var isOverlay = ConditionalCompilation.IncludesOpenVR && !args.Contains("--no-overlay");
-var registerManifest = ConditionalCompilation.RegisterManifest ? !args.Contains("--no-register-manifest") : args.Contains("--register-manifest");
+new HViewProgram(args).Run();
 
-// Create a desktop window stylized as being the overlay version.
-// This does nothing when the --overlay arg is set.
-var simulateWindowlessStyle = args.Contains("--simulate-windowless");
-
-// Allow this app to run in both Overlay and Normal mode as separately managed instances.
-var serviceName = isOverlay ? $"{HVApp.AppName}-Overlay" : $"{HVApp.AppName}-Windowed";
-
-if (!Directory.Exists(SaveUtil.GetUserDataFolder()))
+internal class HViewProgram
 {
-    Console.WriteLine($"Save directory does not exist. Creating...");
-    Directory.CreateDirectory(SaveUtil.GetUserDataFolder());
-}
-var config = SavedData.OpenConfig();
+    // ReSharper disable InconsistentNaming
+    private readonly SavedData config;
+    private readonly FakeVRCOSC fakeVrcOptional;
+    private readonly HOsc oscClient;
+    private readonly HQuery oscQuery;
+    private readonly HNSteamworks steamworksOptional;
+    private readonly HVExternalService externalService;
+    private readonly HVRoutine routine;
+    private readonly HVOpenVRThread ovrThreadOptional;
+    private readonly Thread uiThread;
+    // ReSharper restore InconsistentNaming
 
-var fakeVrcOptional = ConditionalCompilation.EnableFakeVrcOsc ? new FakeVRCOSC() : null;
-
-var oscPort = HOsc.RandomOscPort();
-var queryPort = HQuery.RandomQueryPort();
-
-var oscClient = new HOsc(oscPort);
-var oscQuery = HQuery.ForVrchat(oscPort, queryPort, serviceName);
-oscQuery.OnTargetOscPortFound += vrcOscPort => oscClient.SetReceiverOscPort(vrcOscPort);
-
-var steamworksOptional = ConditionalCompilation.IncludesSteamworks ? new HNSteamworks() : null;
-
-var messageBox = new HMessageBox();
-var externalService = new HVExternalService();
-var routine = new HVRoutine(oscClient, oscQuery, messageBox, externalService, steamworksOptional, fakeVrcOptional);
-
-var ovrThreadOptional = isOverlay ? new HVOpenVRThread(routine, registerManifest) : null;
-
-// Start services
-oscClient.Start();
-oscQuery.Start();
-externalService.Start();
-steamworksOptional?.Start();
-fakeVrcOptional?.Start();
-routine.Start();
-
-void WhenWindowClosed()
-{
-    routine.Finish();
-    oscQuery.Finish();
-    oscClient.Finish();
-    ovrThreadOptional?.Finish();
-    fakeVrcOptional?.Finish();
-}
-
-// Start the VR or UI thread.
-new Thread(() =>
-{
-    if (isOverlay)
+    public HViewProgram(string[] arguments)
     {
-        Console.WriteLine("Starting as a hybrid desktop / VR app.");
-        new HVOvrStarter(ovrThreadOptional, WhenWindowClosed).Run();
+        var isOverlay = ConditionalCompilation.IncludesOpenVR && !arguments.Contains("--no-overlay");
+        
+        var registerManifest = ConditionalCompilation.RegisterManifest ? !arguments.Contains("--no-register-manifest") : arguments.Contains("--register-manifest");
+
+        // Create a desktop window stylized as being the overlay version.
+        // This does nothing when the --overlay arg is set.
+        var simulateWindowlessStyle = arguments.Contains("--simulate-windowless");
+
+        // Allow this app to run in both Overlay and Normal mode as separately managed instances.
+        var serviceName = isOverlay ? $"{HVApp.AppName}-Overlay" : $"{HVApp.AppName}-Windowed";
+
+        if (!Directory.Exists(SaveUtil.GetUserDataFolder()))
+        {
+            Console.WriteLine($"Save directory does not exist. Creating...");
+            Directory.CreateDirectory(SaveUtil.GetUserDataFolder());
+        }
+        config = SavedData.OpenConfig();
+
+        fakeVrcOptional = ConditionalCompilation.EnableFakeVrcOsc ? new FakeVRCOSC() : null;
+
+        var oscPort = HOsc.RandomOscPort();
+        var queryPort = HQuery.RandomQueryPort();
+
+        oscClient = new HOsc(oscPort);
+        oscQuery = HQuery.ForVrchat(oscPort, queryPort, serviceName);
+        oscQuery.OnTargetOscPortFound += vrcOscPort => oscClient.SetReceiverOscPort(vrcOscPort);
+
+        steamworksOptional = ConditionalCompilation.IncludesSteamworks ? new HNSteamworks() : null;
+
+        var messageBox = new HMessageBox();
+        externalService = new HVExternalService();
+        
+        routine = new HVRoutine(oscClient, oscQuery, messageBox, externalService, steamworksOptional, fakeVrcOptional);
+        
+        ovrThreadOptional = isOverlay ? new HVOpenVRThread(routine, registerManifest) : null;
+        
+        uiThread = new Thread(() =>
+        {
+            if (isOverlay)
+            {
+                Console.WriteLine("Starting as a hybrid desktop / VR app.");
+                new HVOvrStarter(ovrThreadOptional, WhenWindowClosed).Run();
+            }
+            else
+            {
+                Console.WriteLine("Starting as a desktop window.");
+                new HVWindow(routine, WhenWindowClosed, simulateWindowlessStyle).Run();
+            }
+        })
+        {
+            CurrentCulture = CultureInfo.InvariantCulture, // We don't want locale-specific numbers
+            CurrentUICulture = CultureInfo.InvariantCulture,
+            Name = isOverlay ? "VR-Thread" : "UI-Thread"
+        };
     }
-    else
+        
+    private void WhenWindowClosed()
     {
-        Console.WriteLine("Starting as a desktop window.");
-        new HVWindow(routine, WhenWindowClosed, simulateWindowlessStyle).Run();
+        routine.Finish();
+        oscQuery.Finish();
+        oscClient.Finish();
+        ovrThreadOptional?.Finish();
+        fakeVrcOptional?.Finish();
     }
-})
-{
-    CurrentCulture = CultureInfo.InvariantCulture, // We don't want locale-specific numbers
-    CurrentUICulture = CultureInfo.InvariantCulture,
-    Name = isOverlay ? "VR-Thread" : "UI-Thread"
-}.Start();
 
-if (fakeVrcOptional != null)
-{
-    fakeVrcOptional.SendAvatarChange();
+    internal void Run()
+    {
+        oscClient.Start();
+        oscQuery.Start();
+        externalService.Start();
+        steamworksOptional?.Start();
+        fakeVrcOptional?.Start();
+        routine.Start();
+        
+        uiThread.Start();
+
+        if (fakeVrcOptional != null)
+        {
+            fakeVrcOptional.SendAvatarChange();
+        }
+
+        routine.MainLoop(); // This call does not return until routine.Finish() is called.
+    }
 }
-
-// Main loop
-routine.MainLoop(); // This call does not return until routine.Finish() is called.
